f334440fffd088b597f8da301bf43e2cb2c52dc1
GAM3GENIE <davidlbernardino@gmail.com>
2025-12-11 07:47:43 -0800

    Week 1

---
+++ Week1/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("First name: Gabriel");
+            System.out.println("Last name: Van Helsing");
+            System.out.println("Street address: 749 Lincoln Street");
+            System.out.println("City: Yuba City");
+            System.out.println("Zip code: 95991");
+        }
+    }
